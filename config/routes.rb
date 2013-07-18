a59f7e91e75ff9b5636deda25e99ef1073d9639b
Raphael Villas <raphael.villas@gmail.com>
2013-07-18 09:57:19 -0400

    Added Task model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 MygovTasks::Application.routes.draw do
+  resources :tasks, except: [:new, :edit]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
